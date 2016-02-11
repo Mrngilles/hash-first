fa9cdfe11bfef5f5b1489935e5a8af542fd8397d
Marin Gilles <mrngilles@gmail.com>
2016-02-11 16:29:25 +0100

    Boilerplate for testing

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='hash_first',
+      version='0.1',
+      description=(''),
+      author='Marin Gilles',
+      author_email='mrngilles@gmail.com',
+      url='',
+      packages=["hash_first"],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"]
+     )
---
+++ tests/test_general.py
@@ -0,0 +1,4 @@
+from hash_first.main import my_function
+
+def test_this():
+    assert(my_function())
