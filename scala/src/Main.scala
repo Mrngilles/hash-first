f54c78fb703d9b7e2af41927417de1e415abc414
tienduccao <duccao1989@gmail.com>
2016-02-12 13:28:13 +0100

    Test reading input

--- scala/src/Main.scala
+++ scala/src/Main.scala
@@ -1,6 +1,13 @@
+import scala.io.Source
+
 /**
   * Created by duccao on 12/02/16.
   */
-object Main extends App{
+object Main extends App {
+
+  val filename = "input/sample.in"
+  for (line <- Source.fromFile(filename).getLines) {
+    println(line)
+  }
 
 }
