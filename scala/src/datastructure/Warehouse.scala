363ff9dbcc83fe377bc837eca40d51fc21977eca
tienduccao <duccao1989@gmail.com>
2016-02-13 10:19:55 +0100

    Warehouse receives list of products and return list of products they can not supply

--- scala/src/datastructure/Warehouse.scala
+++ scala/src/datastructure/Warehouse.scala
@@ -1,6 +1,6 @@
 package datastructure
 
-import scala.util.control.Breaks._
+import scala.collection.mutable.ListBuffer
 
 /**
   * Created by duccao on 12/02/16.
@@ -8,16 +8,36 @@ import scala.util.control.Breaks._
 class Warehouse extends ProductContainer {
   override def toString = s"Warehouse(id $id, ($x, $y), products = $products)"
 
-  def canServe(order: Order): Boolean = {
-    var canServe = true
-    breakable {
-      order.products.foreach { onDemandProduct =>
-        canServe = this.products
-          .count(product => product.equals(onDemandProduct) && product.quantity >= onDemandProduct.quantity) > 0
-        if (!canServe) break
+  /**
+    * @param onDemandProducts
+    * @return list supplied products and remaining products
+    */
+  def supply(onDemandProducts: Iterable[Product]): (List[Product], List[Product]) = {
+    val remainingProducts = new ListBuffer[Product]
+    val suppliedProducts = new ListBuffer[Product]
+    onDemandProducts.foreach { product =>
+      if (this.products.contains(id)) {
+        val inStockQuantity = this.products(id).quantity
+        val onDemandQuantity = product.quantity
+
+        // warehouse doesn't have enough quantity to supply
+        if (inStockQuantity < onDemandQuantity) {
+          remainingProducts += new Product(product.id, onDemandQuantity - inStockQuantity, product.weight)
+          suppliedProducts += new Product(product.id, inStockQuantity, product.weight)
+
+          // this product is no longer available in warehouse
+          this.products.remove(id)
+        } else {
+          suppliedProducts += new Product(product.id, onDemandQuantity, product.weight)
+
+          // update quantity of this product in stock
+          this.products += (id -> new Product(product.id, inStockQuantity - onDemandQuantity, product.weight))
+        }
+      } else {
+        remainingProducts += product
       }
     }
 
-    canServe
+    (suppliedProducts.toList, remainingProducts.toList)
   }
 }
